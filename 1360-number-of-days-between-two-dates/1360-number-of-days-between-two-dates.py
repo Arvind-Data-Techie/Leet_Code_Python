d8cd68383dc6a4831f7fd911bfa882095e9aa1ad
Arvind G <64821617+Arvind-Data-Techie@users.noreply.github.com>
2024-02-18 11:44:10 +0530

    Time: 31 ms (89.47%), Space: 16.5 MB (82.40%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
@@ -2,12 +2,25 @@ from datetime import datetime
 
 class Solution:
     
+       
     def daysBetweenDates(self, date1: str, date2: str) -> int:
-        year, month, day = map(int, date1.split('-'))
-        date_1=date(year,month,day)
-        year, month, day = map(int, date2.split('-'))
-        date_2 = date(year, month, day)
-        return abs((date_1-date_2).days)
+        
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        
+        
+        def isLeapYear(year: int) -> bool:
+             return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+        def daysFrom1971(date: str) -> int:
+            year, month, day = map(int, date.split('-'))
+            return 365 * (year - 1971) + sum(map(isLeapYear, range(1971, year))) + \
+              sum(days[:month]) + day + (month > 2 and isLeapYear(year))
+        
+        return abs(daysFrom1971(date1) - daysFrom1971(date2))
+
+
+    
+    
         
     
     
