70a27400e6af6d41b9829b9f11639e1775383b2d
Arvind G <64821617+Arvind-Data-Techie@users.noreply.github.com>
2024-02-25 19:04:04 +0530

    Time: 49 ms (49.20%), Space: 16.6 MB (82.37%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,49 +2,21 @@ class Solution:
     
     def romanToInt(self, s: str) -> int:
         
-        val=0
-        
-        if 'IV' in s:
-            val += 4
-            s = s.replace('IV', '')
-
-        if 'IX' in s:
-            val += 9
-            s = s.replace('IX', '')
-
-        if 'XL' in s:
-            val += 40
-            s = s.replace('XL', '')
-
-        if 'XC' in s:
-            val += 90
-            s = s.replace('XC', '')
-
-        if 'CD' in s:
-            val += 400
-            s = s.replace('CD', '')
-
-        if 'CM' in s:
-            val += 900
-            s = s.replace('CM', '')
-
-        
-        for i in s:
-            if i == 'I':
-                val += 1
-            elif i == 'V':
-                val += 5
-            elif i == 'X':
-                val += 10
-            elif i == 'L':
-                val += 50
-            elif i == 'C':
-                val += 100
-            elif i == 'D':
-                val += 500
-            elif i == 'M':
-                val += 1000
-                
-        return val
+        vals = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        sum = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            sum += vals[char]
+        return sum
             
         
