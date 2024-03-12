d127e0e73cf85e466b0535715b560a1381cea819
Arvind G <64821617+Arvind-Data-Techie@users.noreply.github.com>
2024-03-12 12:15:50 +0530

    Time: 41 ms (66.67%), Space: 17.2 MB (24.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,8 +1,6 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
-        if len(s) != len(t):
-            return False
+
 
         if [*map(s.index, s)]==[*map(t.index, t)]:
             return True
