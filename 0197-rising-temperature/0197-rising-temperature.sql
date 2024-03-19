ae4e3e3f411cf8de7dcc386fca1297748d47b42e
Arvind G <64821617+Arvind-Data-Techie@users.noreply.github.com>
2024-03-19 16:14:16 +0530

    Time: 1251 ms (32.80%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,4 @@
-SELECT a.Id AS Id
-FROM Weather a
-JOIN Weather b
-ON a.RecordDate = DATEADD(DAY, 1, b.RecordDate)
-WHERE a.Temperature > b.Temperature
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON DATEDIFF(DAY, w2.recordDate, w1.recordDate) = 1
+AND w1.Temperature > w2.Temperature
