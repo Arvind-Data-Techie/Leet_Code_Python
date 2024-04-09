9681dfe895f23e24b0f9ff12c6c3c80f4f03a400
Arvind G <64821617+Arvind-Data-Techie@users.noreply.github.com>
2024-04-09 19:22:48 +0530

    Time: 4455 ms (9.47%), Space: 0B (100.00%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+SELECT customer_id
+FROM   customer
+GROUP  BY customer_id
+HAVING Count(DISTINCT product_key) = (SELECT
+       Count(DISTINCT product_key) AS totl_prdct
+                                      FROM   product) 
